84ff9be28db7682bf9e30d43e686485e2967e70e
Jay Kumar <jay.d.kumar@gmail.com>
2021-01-26 11:15:07 -0800

    Updated port: 3002

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 const HomeController = require('./controllers/HomeController.js');
 

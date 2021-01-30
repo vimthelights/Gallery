89a6acd369fe321eb0ee0ca57012894680f20646
Jay Kumar <jay.d.kumar@gmail.com>
2021-01-29 21:26:27 -0800

    hopefully final button move

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -53,7 +53,7 @@ const SmallImage = styled.img`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: right;
-  right: 400px;
+  right: 440px;
   align-items: space-between;
   width: 250px;
   height: 200px;
@@ -90,7 +90,7 @@ const ForSaleContainer = styled.div`
   width: 80px;
   height: 20px;
   margin-top: 10px;
-  left: 444px;
+  left: 484px;
   z-index: 2;
 `;
 
